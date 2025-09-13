8c551378f51ba3660b501ee0bd848d076fe9626d
adiidubey <dubeyaditya397@gmail.com>
2025-09-13 20:55:56 +0530

    firebase setup

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "vingo-food-delivery.firebaseapp.com",
-  projectId: "vingo-food-delivery",
-  storageBucket: "vingo-food-delivery.firebasestorage.app",
-  messagingSenderId: "693314883513",
-  appId: "1:693314883513:web:4840c943ecf3fb61a33068"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "foodzy-88854.firebaseapp.com",
+  projectId: "foodzy-88854",
+  storageBucket: "foodzy-88854.firebasestorage.app",
+  messagingSenderId: "880693276790",
+  appId: "1:880693276790:web:cc0756eed1f807a8f9af57",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth=getAuth(app)
-export {app,auth}
+const auth = getAuth(app);
+
+export { app, auth };
